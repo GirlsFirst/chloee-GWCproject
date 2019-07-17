d0f77407edc4b49a6ca0ac5e8ca83ec025f11c5d
chloeegong <34522867+chloeegong@users.noreply.github.com>
2019-07-17 09:53:36 -0700

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,9 @@
+//function changeImage() {
+  //var onclick = document.getElementById("compImage1");
+  //if (image.src.match("compImage2")) {
+    //image.src = "image.compImage1";
+  //}
+  //else {
+    //image.src = "compImage2"
+  //}
+//}
